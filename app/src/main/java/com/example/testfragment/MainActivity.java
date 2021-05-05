e7b1dbec1125babfa6acd9ac7b0fc4075dc358b2
ghadaghanney <ghada@ghanney.(none)>
2021-05-05 14:21:39 +0100

    first commit

---
+++ app/src/main/java/com/example/testfragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+                          implements SecondFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
